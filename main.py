effb60963d6da0b55f30fc70f0c312e091870bd6
chatton <cianhatton@gmail.com>
2021-05-08 21:00:23 +0100

    Added _ensure_lists function to generate empty boards

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ import json
 from trello import TrelloClient
 
 CONFIG_PATH = os.path.expanduser("~/.trello-tasks")
+DESIRED_LISTS = ("TODO", "In Progress", "Done")
 
 
 class TrelloTask:
     def __init__(self, client: TrelloClient, board_name: str):
         self.client = client
         self.active_board = self._get_board_by_name(board_name)
+        self._ensure_lists()
 
     def _get_board_by_name(self, name: str):
         return next(board for board in self.client.list_boards() if board.name == name)
+
+    def _ensure_lists(self):
+        open_trello_lists = [trello_list for trello_list in self.active_board.list_lists() if not trello_list.closed]
+        list_names = [tl.name for tl in open_trello_lists]
+        for list_name in DESIRED_LISTS:
+            if list_name not in list_names:
+                self.active_board.add_list(name=list_name)
+                continue
 
 
 def _load_config():
@@ -32,8 +42,8 @@ def main() -> int:
     )
 
     trello_task = TrelloTask(client, config["board"])
-    return 0
 
+    return 0
 
 
 if __name__ == "__main__":
